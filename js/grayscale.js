6e97c3ce503d54fa04177f25b746d58052236532
Bastien Picharles <bastien.picharles@gmail.com>
2016-06-25 01:26:11 +0200

    gallerie et partage

--- js/grayscale.js
+++ js/grayscale.js
@@ -25,6 +25,29 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+    $('#teasers .video-thumb a').click(function(event) {
+        event.preventDefault();
+        var url = $(this).attr('href');
+        $('#teaser-wrapper iframe').attr('src', url);
+    });
+
+    $('.popup').click(function(event) {
+        var width  = 575,
+            height = 400,
+            left   = ($(window).width()  - width)  / 2,
+            top    = ($(window).height() - height) / 2,
+            url    = this.href,
+            opts   = 'status=1' +
+             ',width='  + width  +
+             ',height=' + height +
+             ',top='    + top    +
+             ',left='   + left;
+
+        window.open(url, 'twitter', opts);
+
+        return false;
+    });
 });
 
 // Closes the Responsive Menu on Menu Item Click
